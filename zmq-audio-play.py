7e53c42b50b9c73a5f8ce2cd9ecc5265465044da
Valentin LE BESCOND <valentin.lebescond@protonmail.com>
2023-02-23 16:58:14 +0100

    remove unused constant

--- zmq-audio-play.py
+++ zmq-audio-play.py
@@ -4,8 +4,6 @@ import sys
 import zmq
 import array
 
-BLOCK_SIZE = 1024
-
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://127.0.0.1:1234")
@@ -17,6 +15,6 @@ while True:
     audio_data_bytes = socket.recv()
 
     audio_data =  array.array('f', audio_data_bytes)
-    # audio_data contains a list of BLOCK_SIZE // 4 floats representing audio values
+    # audio_data contains a list of floats representing audio values
 
     out_buffer.write(audio_data_bytes)
